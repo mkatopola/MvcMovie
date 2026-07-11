1adf605a623d3a9936eb11ea63cbc507632fd341
mkatopola <moffatkatopola@gmail.com>
2026-07-11 12:45:59 +0200

    Changed movie listing title to "Moffat Movies"

--- obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f10d843193a94811f168edbabef986f951e21d1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91bb2689c5cf00160751926e9b2cb73ad7650313")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
